76c1e54e77eb5458e714b81f5dc002094f3b1394
sudatra <mail2sudatra@gmail.com>
2024-09-15 18:23:57 +0530

    added driver-store in zustand and created markers for nearby driver on map

--- store/index.ts
+++ store/index.ts
@@ -1,4 +1,4 @@
-import { LocationStore } from "@/types/type";
+import { DriverStore, LocationStore, MarkerData } from "@/types/type";
 import { create } from "zustand";
 
 export const useLocationStore = create<LocationStore>((set) => ({
@@ -29,3 +29,19 @@ export const useLocationStore = create<LocationStore>((set) => ({
   }
 }));
 
+export const useDriverStore = create<DriverStore>((set) => ({
+  drivers: [] as MarkerData[],
+  selectedDriver: null,
+
+  setSelectedDriver: (driverId: number) => set(() => ({
+    selectedDriver: driverId
+  })),
+
+  setDrivers: (drivers: MarkerData[]) => set(() => ({
+    drivers: drivers
+  })),
+
+  clearSelectedDriver: () => set(() => ({
+    selectedDriver: null
+  }))
+}));
